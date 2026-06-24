de214752a0cd7220e6f63190f77f3b5a5c93effa
rafaelsamico <rsamicod@gmail.com>
2026-06-24 10:25:09 -0300

    feat(router): add routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Pacientes from "./pages/Pacientes"
+import Profissionais from "./pages/Profissionais"
+
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pacientes" element={<Pacientes />} />
+        <Route path="/profissionais" element={<Profissionais />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
